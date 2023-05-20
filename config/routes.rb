935780f8ebc5b746e796bad74ea84176d1517587
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-20 13:05:39 +0000

    [add]フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  devise_for :users
   root to: "homes#top"
@@ -10,6 +9,10 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
