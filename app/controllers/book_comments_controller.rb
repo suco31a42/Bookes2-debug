f0d9ac0c284e8880c28ddb021c5af9fb98518408
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-06-01 10:03:52 +0000

    [add]いいね機能非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,7 +5,6 @@ class BookCommentsController < ApplicationController
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
     @comment.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
